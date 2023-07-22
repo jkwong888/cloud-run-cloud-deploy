7c64c9f138e7973246d375a2bab245b9d0d85a17
jkwng <jkwng@google.com>
2023-07-22 17:58:04 -0400

    remove docker build cache

--- terraform/gcr-iam.tf
+++ terraform/gcr-iam.tf
@@ -5,6 +5,6 @@ data "google_project" "registry_project" {
 # take the GKE SA and allow storage object browser on the image registry bucket
 resource "google_storage_bucket_iam_member" "github_push" {
     bucket  = format("artifacts.%s.appspot.com", data.google_project.registry_project.project_id)
-    role    = "roles/storage.objectAdmin"
+    role    = "roles/storage.admin"
     member  = format("serviceAccount:%s", google_service_account.github_sa.email)
 }
