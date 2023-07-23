50552af920632f56d6658307cce5a21577ae4c47
Jeffrey Kwong <jkwong888@users.noreply.github.com>
2023-07-23 10:26:43 -0400

    add new route (#1)
    * add new route
    
    * asdfasd
    
    * trigger an action
    
    * aasdf
    
    * trigger workflow
    
    * use port env variable
    
    * trigger workflow
    
    * trigger workflow
    
    * trigger workflow
    
    * trigger workflow
    
    * trigger workflow
    
    * make service public
    
    * trigger workflow

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,21 @@
 import express, { Application, Request, Response } from 'express'
-
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient();
-
 const app: Application = express()
-
-const port: number = 3001
+const port: number = Number(process.env.PORT || 3001);
 
 app.get('/toto', (req: Request, res: Response) => {
-    res.send('Hello toto')
-})
+    res.send('Hello toto');
+});
+
+app.get('/hello', (req: Request, res: Response) => {
+    res.send('Hello toto');
+});
 
 app.listen(port, "0.0.0.0", function () {
     console.log(`App is listening on port ${port} !`)
-})
+});
 
 /**
  * Do stuff and exit the process
@@ -22,9 +23,9 @@ app.listen(port, "0.0.0.0", function () {
  */
 function signalHandler(_signal: any) {
     // do some stuff here
-    process.exit()
+    process.exit();
 }
 
-process.on('SIGINT', signalHandler)
-process.on('SIGTERM', signalHandler)
-process.on('SIGQUIT', signalHandler)
+process.on('SIGINT', signalHandler);
+process.on('SIGTERM', signalHandler);
+process.on('SIGQUIT', signalHandler);
